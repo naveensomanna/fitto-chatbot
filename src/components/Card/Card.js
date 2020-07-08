5c14d7c65e91ab4716a142abfacce625f1a98ee4
naveenkumar <naveen.k@highpeaksw.com>
2020-07-08 10:01:42 +0530

    initial commit

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+const Card = () => {
+  return (
+    <section className={styles.cardContainer}>
+      <header>
+        <p>Virtual assistant</p>
+      </header>
+    </section>
+  );
+};
+
+export default Card;
